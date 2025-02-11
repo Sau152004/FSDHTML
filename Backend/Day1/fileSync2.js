2c4489d50c7df162b1e8ff0c5371c6194bc55072
sau152004 <ysaurabh0704@gmail.com>
2025-02-11 15:28:57 +0530

    Backend Day1 work done

---
+++ Backend/Day1/fileSync2.js
@@ -0,0 +1,12 @@
+const {myReadFile, myWriteFile, myAppendFile, myDeletefile, username}=require("./fileSync1");
+myReadFile();
+
+const data="My FSD Class";
+ myWriteFile(data);
+
+ myReadFile();
+ myAppendFile(data);
+ myReadFile();
+
+ myDeletefile("dummy.txt");
+ console.log("user name=",username);
